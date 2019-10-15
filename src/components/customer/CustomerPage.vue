f0e1651d884c1f63d3822a8424ce7dcd500ce477
Michael <michael.isvy@online-pajak.com>
2019-10-15 08:46:48 +0800

    added sample dialog that opens when clicking on Customer>Edit

--- src/components/customer/CustomerPage.vue
+++ src/components/customer/CustomerPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="mx-auto" max-width="800">
-    <v-data-table 
-      :headers="customerList.headers" 
+    <v-data-table
+      :headers="customerList.headers"
       :items="customerList.data"
       @click:row="displayCustomerDetails"
     >
@@ -11,19 +11,18 @@
         </v-toolbar>
       </template>
       <template v-slot:item.action="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click.stop="editItem(item)"
-        >
-          edit
-        </v-icon>
-        <v-icon
-          small
-          @click.stop="deleteItem(item)"
-        >
-          delete
-        </v-icon>
+        <v-icon small class="mr-2" @click.stop="editItem(item)">edit</v-icon>
+        <v-icon small @click.stop="deleteItem(item)">delete</v-icon>
+        <v-dialog v-model="editDialog" max-width="290">
+          <v-card>
+            <v-card-title class="headline">Use Google's location service?</v-card-title>
+            <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="green darken-1" text @click="editDialog = false">Agree</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </template>
     </v-data-table>
   </v-card>
@@ -34,20 +33,25 @@ import * as customerService from "@/service/customerService";
 export default {
   data: function() {
     return {
-      customerList: Object
+      customerList: Object,
+      editDialog: false
     };
   },
   created() {
     this.customerList = customerService.getCustomerList();
   },
   methods: {
-    editItem(item) {
-      alert("hellooo " + item.firstName);
+    editItem() {
+      this.editDialog = true;
     },
     deleteItem(item) {
-      const message = "are you sure you want to delete customer " + item.firstName + " " + item.lastName + "?";
+      const message =
+        "are you sure you want to delete customer " +
+        item.firstName +
+        " " +
+        item.lastName +
+        "?";
       confirm(message) && customerService.deleteById(item.id);
-
     },
     displayCustomerDetails(item) {
       alert(item.firstName + "  ");
